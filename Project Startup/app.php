32ef00ba4c53dd6a5450566bc980982d2552242d
Saptarshi Datta <57251177+Almunia777@users.noreply.github.com>
2021-05-20 20:16:35 +0530

    Add files via upload

---
+++ Project Startup/app.php
@@ -0,0 +1,8 @@
+<?php
+$username = $_POST['full-name'];
+$email =  $_POST['your-email'];
+$password = $_POST['password'];
+$phn = $_POST['Mobile'];
+$address = $_POST['Address'];
+$city = $_POST['City'];
+$pin = $_POST['PIN'];
